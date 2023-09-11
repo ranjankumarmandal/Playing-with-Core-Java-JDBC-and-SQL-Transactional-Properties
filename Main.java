a0d8ab04bc66b9b58b459d6be406b003acb5f058
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-12 00:10:20 +0530

    core-java: jdbc: implemented getAllPerson implementaion with executeQuery method

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("welcome to core java - JDBC!");
+public class Main implements JdbcInterface {
+    private Connection connection;
+
+    @Override
+    public List<Person> getAllPerson(){
+        List<Person> list = new ArrayList<>();
+        Statement statement  = null;
+
+        try{
+            statement =  connection.createStatement();
+            ResultSet resultSet =statement.executeQuery("select * from person");
+            while(resultSet.next()){
+                Person p = new Person(resultSet.getString("name"), resultSet.getInt("id"));
+                list.add(p);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return list;
     }
 }
