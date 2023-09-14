626963e497abc593d18a933e675619346207bdd2
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-14 09:22:29 +0530

    core-java: jdbc: implemented updatePerson implementaion preparedStatement method

--- Main.java
+++ Main.java
@@ -25,4 +25,41 @@ public class Main implements JdbcInterface {
 
         return list;
     }
+
+    @Override
+    public int updatePerson(int id, String name){
+        Boolean autocommit  = null;
+        
+        try {
+            autocommit = connection.getAutoCommit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        int result =0 ;
+
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement ps = connection.prepareStatement("update person set name =? where id = ?");
+            ps.setString(1, name);
+            ps.setInt(2, id);
+            result = ps.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                connection.setAutoCommit(autocommit);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return result;
+    }
 }
