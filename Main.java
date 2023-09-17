e2e979af7ae95a699de3ca93a83ad61d567be7fc
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-17 13:45:29 +0530

    jdbc: core-java: completed all the query execution with preparedStatement, executeQuery and other important java classes without any framework

--- Main.java
+++ Main.java
@@ -62,4 +62,64 @@ public class Main implements JdbcInterface {
 
         return result;
     }
+
+    @Override
+    public Person addPersonWithPreparedStatement(Person person){
+        PreparedStatement ps  = null;
+        try{
+            ps =  connection.prepareStatement("insert into person (name, id) VALUES ( ?, ?) ;");
+            ps.setString(1, person.getName());
+            ps.setInt(2, person.getId());
+//            ps.setInt(3, person.getId());
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return person;
+
+    }
+
+    @Override
+    public Person addPerson(Person person) {
+        Statement statement  = null;
+        try{
+            statement =  connection.createStatement();
+            boolean result = statement.execute("insert into person (name, id) VALUES ('" + person.getName() +"' ," +person.getId() +");");
+            logger.info("the result of the above query is {}" ,result);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return person;
+
+    }
+
+    public void createTablePerson(){
+        Statement statement =null;
+
+        try {
+             statement = connection.createStatement();
+            statement.execute("create table if not exists person (name varchar(30), id int )");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
